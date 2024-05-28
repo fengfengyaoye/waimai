4fa4eb40247ff82b6bbf7a92857a1cef87e20b2a
fengfengyaoye <hello@163.com>
2024-05-28 12:15:20 +0800

    用户数据统计功能代码开发

--- sky-server/src/main/java/com/sky/mapper/UserMapper.java
+++ sky-server/src/main/java/com/sky/mapper/UserMapper.java
@@ -4,6 +4,8 @@ import com.sky.entity.User;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+import java.util.Map;
+
 @Mapper
 public interface UserMapper {
 
@@ -28,4 +30,11 @@ public interface UserMapper {
      */
     @Select("select * from sky_take_out.user where id=#{id}")
     User getById(Long userId);
+
+    /**
+     * 统计用户数据
+     * @param newUserMap
+     * @return
+     */
+    Integer countByMap(Map newUserMap);
 }
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -2,8 +2,10 @@ package com.sky.service.impl;
 
 import com.sky.entity.Orders;
 import com.sky.mapper.OrderMapper;
+import com.sky.mapper.UserMapper;
 import com.sky.service.ReportService;
 import com.sky.vo.TurnoverReportVO;
+import com.sky.vo.UserReportVO;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,6 +26,9 @@ public class ReportServiceImpl implements ReportService {
     //查询的是订单表
     @Autowired
     private OrderMapper orderMapper;
+
+    @Autowired
+    private UserMapper userMapper;
     /**
      * 统计指定时间区间内的营业额数据
      * @param begin
@@ -40,7 +45,6 @@ public class ReportServiceImpl implements ReportService {
             begin = begin.plusDays(1);
             dateList.add(begin);
         }
-        String join = StringUtils.join(dateList, ",");
 
         //存放每天营业额
         List<Double> turnoverList = new ArrayList<>();
@@ -63,11 +67,64 @@ public class ReportServiceImpl implements ReportService {
             turnoverList.add(turnover);
         }
 
-
         return TurnoverReportVO
                 .builder()
                 .dateList(StringUtils.join(dateList, ","))//将list集合按照","进行分割 并转换为字符串
                 .turnoverList(StringUtils.join(turnoverList, ","))//将list集合按照","进行分割 并转换为字符串
+                .build();
+    }
+
+    /**
+     * 用户统计
+     * @param begin
+     * @param end
+     * @return
+     */
+    public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
+        //用于存在从begin到end范围内每天日期
+        List<LocalDate> dateList = new ArrayList<>();
+
+        dateList.add(begin);
+        while (!begin.equals(end)){
+            //日期计算 指定日期的后一天对应的日期
+            begin = begin.plusDays(1);
+            dateList.add(begin);
+        }
+
+        //存放每天新增用户
+        List<Integer> newUserList = new ArrayList<>();
+        //存放每天的总用户数量
+        List<Integer> totalUserList = new ArrayList<>();
+
+        //需要查user表 统计用户
+        //每天新增
+        //# select sum(amount) from user where create_time > begin and crate_time < end;
+        //每天新增
+        //# select sum(amount) from user where crate_time < end;
+
+        for (LocalDate date : dateList) {
+            //查询data日期对应的营业额数据 营业额:状态为"已完成"的订单金额合计
+
+            LocalDateTime beginTime = LocalDateTime.of(date, LocalTime.MIN);//2024-05-25 00:00:00
+            LocalDateTime endTime = LocalDateTime.of(date, LocalTime.MAX);//2024-05-25 23:59:59
+
+            Map map = new HashMap();
+
+            map.put("end",endTime);
+            Integer totalUser = userMapper.countByMap(map);
+
+            map.put("begin",beginTime);
+            Integer newUser = userMapper.countByMap(map);
+            //这里如果没有数据不用转换 因为count查询的就是0
+            newUserList.add(newUser);
+            totalUserList.add(totalUser);
+        }
+
+        return UserReportVO
+                .builder()
+                .dateList(StringUtils.join(dateList, ","))
+                .totalUserList(StringUtils.join(totalUserList, ","))
+                .newUserList(StringUtils.join(newUserList, ","))
                 .build();
     }
 }
