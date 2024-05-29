540f51f0b8227ffd1e34e17f80056d63fb12cb2a
fengfengyaoye <hello@163.com>
2024-05-29 11:52:52 +0800

    导出运营数据报表功能代码开发

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -9,6 +9,7 @@ import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.lettuce.core.output.ListOfGenericMapsOutput;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
@@ -35,6 +37,7 @@ public class ReportController {
      * @return
      */
     @GetMapping("/turnoverStatistics")
+    @ApiOperation("营业额统计")
     public Result<TurnoverReportVO> turnoverStatistics(
             //设置日期格式
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
@@ -51,6 +54,7 @@ public class ReportController {
      * @return
      */
     @GetMapping("/userStatistics")
+    @ApiOperation("用户统计")
     public Result<UserReportVO> userStatistics(
             //设置日期格式
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
@@ -66,6 +70,7 @@ public class ReportController {
      * @return
      */
     @GetMapping("/ordersStatistics")
+    @ApiOperation("订单统计")
     public Result<OrderReportVO> orderStatistics(
             //设置日期格式
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
@@ -81,6 +86,7 @@ public class ReportController {
      * @return
      */
     @GetMapping("/top10")
+    @ApiOperation("销量排名top10")
     public Result<SalesTop10ReportVO> top10(
             //设置日期格式
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
@@ -90,5 +96,15 @@ public class ReportController {
     }
 
 
+    /**
+     * 导出运营数据报表
+     * @param response
+     */
+    @GetMapping("/export")
+    @ApiOperation("导出运营数据报表")
+    //下载到浏览器 需要有输出流 输出流需要HttpServletResponse获得
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessData(response);
+    }
 
 }
